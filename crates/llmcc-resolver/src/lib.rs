246a9cf5e4af39d40395345e2add47ce752df7fc
Qiang Zhang <allenanswerzq@gmail.com>
2025-11-12 22:38:25 -0800

    starts refactor llmcc-resolver

--- crates/llmcc-resolver/src/lib.rs
+++ crates/llmcc-resolver/src/lib.rs
@@ -1,12 +1,15 @@
 pub mod binder;
-pub mod call_target;
+// pub mod call_target;  // TODO: Create when implementing language-specific collectors
 pub mod collector;
-mod type_expr;
+// mod type_expr;  // TODO: Create when implementing language-specific collectors
 
 pub use binder::BinderCore;
-pub use collector::{
-    apply_collected_symbols, apply_symbol_batch, collect_symbols_batch, CallCollection,
-    ClassCollection, CollectedSymbols, CollectionResult, CollectorCore, DescriptorCollection,
-    EnumCollection, FunctionCollection, ImplCollection, ImportCollection, ScopeSpec,
-    StructCollection, SymbolSpec, VariableCollection,
-};
+pub use collector::CollectorCore;
+
+// Language-specific collection types will be exported here once implemented
+// pub use collector::{
+//     apply_collected_symbols, apply_symbol_batch, collect_symbols_batch, CallCollection,
+//     ClassCollection, CollectedSymbols, CollectionResult, CollectorCore, DescriptorCollection,
+//     EnumCollection, FunctionCollection, ImplCollection, ImportCollection, ScopeSpec,
+//     StructCollection, SymbolSpec, VariableCollection,
+// };
