a0b31ff7818b8d2e6e6d258727eff58b01d76de4
allenanswerzq <allenanswerzq@gmail.com>
2025-11-24 11:52:33 -0800

    llmcc-rust: more refactor and improvements

--- crates/llmcc-rust/src/collect.rs
+++ crates/llmcc-rust/src/collect.rs
@@ -7,9 +7,9 @@ use llmcc_resolver::{CollectorScopes, ResolverOption};
 
 use std::collections::HashMap;
 
+use crate::LangRust;
 use crate::token::AstVisitorRust;
 use crate::util::{parse_crate_name, parse_file_name, parse_module_name};
-use crate::{LangRust, RUST_PRIMITIVES};
 
 #[derive(Debug)]
 pub struct CollectorVisitor<'tcx> {
@@ -46,12 +46,6 @@ impl<'tcx> CollectorVisitor<'tcx> {
             let child = unit.hir_node(*child_id);
             Self::type_name_from_node(unit, &child)
         })
-    }
-
-    fn initialize(&self, node: &HirNode<'tcx>, scopes: &mut CollectorScopes<'tcx>) {
-        for prim in RUST_PRIMITIVES {
-            scopes.lookup_or_insert_global(prim, node, SymKind::Primitive);
-        }
     }
 
     /// Declare a symbol from a named field in the AST node
@@ -150,26 +144,28 @@ impl<'tcx> CollectorVisitor<'tcx> {
     ) {
         // Check visibility
         let (is_pub, is_pub_crate) = Self::check_visibility(unit, node);
-        let is_top_level = scopes.top().map(|s| s.id()) == Some(scopes.globals().id());
+        let at_global = scopes.top().map(|s| s.id()) == Some(scopes.globals().id());
 
-        let mut parent_is_global = is_top_level;
-        if !is_top_level
-            && let Some(scope) = scopes.top()
+        if is_pub {
+            if !at_global {
+                scopes.globals().insert(sym);
+            }
+            sym.set_is_global(true);
+            return;
+        }
+
+        if is_pub_crate {
+            if !at_global {
+                scopes.globals().insert(sym);
+            }
+            return;
+        }
+
+        if let Some(scope) = scopes.top()
             && let Some(parent_sym) = scope.opt_symbol()
             && parent_sym.is_global()
         {
-            parent_is_global = true;
-        }
-
-        if parent_is_global {
-            if is_pub {
-                if !is_top_level {
-                    scopes.globals().insert(sym);
-                }
-                sym.set_is_global(true);
-            } else if is_pub_crate && !is_top_level {
-                scopes.globals().insert(sym);
-            }
+            scopes.globals().insert(sym);
         }
     }
 
@@ -637,7 +633,7 @@ impl<'tcx> AstVisitorRust<'tcx, CollectorScopes<'tcx>> for CollectorVisitor<'tcx
             self.declare_symbol_from_field(unit, node, scopes, SymKind::Const, LangRust::field_name)
             && let Some(owner) = namespace.opt_symbol()
         {
-            owner.add_dependency(sym);
+            owner.add_dependency(sym, None);
         }
         self.visit_children(unit, node, scopes, namespace, parent);
     }
@@ -808,7 +804,6 @@ pub fn collect_symbols<'tcx>(
     let mut scopes = CollectorScopes::new(cc, unit.index, scope_stack, unit_globals);
 
     let mut visit = CollectorVisitor::new();
-    visit.initialize(node, &mut scopes);
     visit.visit_node(&unit, node, &mut scopes, unit_globals, None);
 
     unit_globals
